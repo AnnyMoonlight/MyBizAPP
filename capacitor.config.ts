110d15ccae7bde6659bd98b0a450cd2eecd37e5f
AnnyMoonlight <vmartina412@gmail.com>
2024-11-19 20:18:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyBiz',
+  webDir: 'www'
+};
+
+export default config;
